0d125c1459c4407b04a691750524a036c041145e
JW <127646014+jw-source@users.noreply.github.com>
2024-09-26 15:03:22 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from Network import Network
-task = '''Breaking News! Donald Trump will launch his own cryptocurrency.'''
-agent_network = Network(task, "Pennsylvania", 15)
-#agent_network.group_chat("round_robin", 1)
-agent_network.simulate("round_robin", 1)
+prompt = '''Kamala Harris is showing up to the Purnell Center today!'''
+population = '''Students at Carnegie Mellon University'''
+num_agents = 10
+agent_network = Network(prompt, population, num_agents)
+agent_network.group_chat("random", 1)
