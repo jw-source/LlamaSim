94516e9dbc6cab7c7c9b956efacd1137f0a306c7
JW <127646014+jw-source@users.noreply.github.com>
2024-10-04 02:08:11 -0400

    Update network.py
    Created a new variable to store complete chat logs, fixed OOP structure a bit

--- src/network.py
+++ src/network.py
@@ -5,15 +5,15 @@ from generate_identity import generate_identities
 import time
 
 class Network:
-    def __init__(self, prompt:str, data:str, num_agents:int):
-        self.prompt = prompt
+    def __init__(self, data:str, num_agents:int, max_context_size:int):
         self.names, self.identities = self._create_identities(data, num_agents)
         self.client = openai.OpenAI(
             base_url='https://api.cerebras.ai/v1',
-            api_key=""
+            api_key="csk-tr33y8n58m4xh4h55yfdpckryddevjktxw4e5jw4m4w66536"
         )
         self.shared_context = []
-        self.max_context_size = 4000
+        self.conversation_logs = []
+        self.max_context_size = max_context_size
         self.num_agents = num_agents
         self.agents = self._init_agents()
         
@@ -27,7 +27,7 @@ class Network:
         
     def _init_agents(self):
         start_time = time.time()
-        agents = [Agent(name=name, prompt=self.prompt, identity=identity, client=self.client) 
+        agents = [Agent(name=name, identity=identity, client=self.client) 
                   for name, identity in zip(self.names, self.identities)]
         end_time = time.time()
         elapsed_time = round(end_time - start_time, 2)
@@ -40,21 +40,25 @@ class Network:
             removed_msg = self.shared_context.pop(0)
             total_length -= len(removed_msg)
 
-    def group_chat(self, chat_type, max_rounds):
+    def group_chat(self, prompt, chat_type, max_rounds):
         round_count = 0
         while round_count < max_rounds: 
             if chat_type == "round_robin":
                 for _, agent in enumerate(self.agents):
+                    agent.prompt = prompt
                     agent_response = agent.chat(self.shared_context)
                     self.shared_context.append(agent.name + ": " + agent_response)
+                    self.conversation_logs.append(agent.name + ": " + agent_response)
                     self._manage_context_size()
                     print(f"\n{agent.name}: {agent_response}")
             elif chat_type == "random":
-                for i in range(len(self.agents)):
+                for _ in range(len(self.agents)):
                     agent = random.choice(self.agents)
+                    agent.prompt = prompt
                     agent_response = agent.chat(self.shared_context)
                     self.shared_context.append(agent.name + ": " + agent_response)
+                    self.conversation_logs.append(agent.name + ": " + agent_response)
                     self._manage_context_size()
                     print(f"\n{agent.name}: {agent_response}")
             round_count += 1
-        return self.shared_context
+        return self.conversation_logs
