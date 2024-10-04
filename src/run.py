561ba4f9940c04b8389bd51dad56288cd1dbefb2
JW <127646014+jw-source@users.noreply.github.com>
2024-10-04 02:11:49 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 from network import Network
 prompt = '''Kamala Harris is showing up to the Purnell Center today!'''
 population = '''Students at Carnegie Mellon University'''
-num_agents = 10
-agent_network = Network(prompt, population, num_agents)
-agent_network.group_chat("random", 1)
+agent_network = Network(population, num_agents=5, max_context_size=4000)
+agent_network.group_chat(prompt, "random", 1)
+
