e9f5f1b66433cb2a2cc525dba3ad3a86207b9c3f
JW <127646014+jw-source@users.noreply.github.com>
2024-10-05 14:28:44 -0400

    Update agent.py
    Added pre and post prediction functions

--- src/agent.py
+++ src/agent.py
@@ -2,12 +2,12 @@ from openai import OpenAI
 
 class Agent:
     def __init__(self, name:str, identity:str, client:OpenAI):
-        self.prompt = None
-        self.name = name
-        self.identity = identity
         self.client = client
         self.model_name = "llama3.1-8b"
+        self.name = name
+        self.identity = identity
         self.backstory = self._create_backstory()
+        self.prompt = None 
         
     def _create_backstory(self):
         response = self.client.chat.completions.create(
@@ -27,7 +27,7 @@ class Agent:
         backstory = response.choices[0].message.content
         return backstory
         
-    def chat(self, conversation_context: list):
+    def chat(self, conversation_context:list):
         response = self.client.chat.completions.create(
             messages=[{
                     "role": "system",
@@ -47,3 +47,55 @@ class Agent:
         )
         agent_reply = response.choices[0].message.content
         return agent_reply
+    
+    def pre_predict(self, question:str):
+        response = self.client.chat.completions.create(
+            messages=[{
+                    "role": "system",
+                    "content": f'''You are an imaginary human with {self.identity}. This is your backstory: {self.backstory}.
+                    Every response should reflect your identity, personal history, experiences, struggles, and values. 
+                    Here's what to consider: Speech Patterns: ALWAYS SPEAK IN 1ST PERSON. Adapt your tone, vocabulary, and speech style to 
+                    align with your background. Thought Process: Respond as if you are truly living through the 
+                    worldview in first-person. Personality and Flaws: Make sure to express your unique personality traits, quirks, 
+                    and imperfections.'''
+                },
+                {
+                    "role": "user",
+                    "content": f"{question}. You can only respond with one word, either 'Yes' or 'No'."
+                }],
+            model=self.model_name,
+            stream=False
+        )
+        agent_reply = response.choices[0].message.content
+        if "Yes" in agent_reply:
+            return 1
+        elif "No" in agent_reply:
+            return 0
+        else:
+            return 2
+    
+    def post_predict(self, prompt:str, question:str):
+        response = self.client.chat.completions.create(
+            messages=[{
+                    "role": "system",
+                    "content": f'''You are an imaginary human with {self.identity}. This is your backstory: {self.backstory}.
+                    Every response should reflect your identity, personal history, experiences, struggles, and values. 
+                    Here's what to consider: Speech Patterns: ALWAYS SPEAK IN 1ST PERSON. Adapt your tone, vocabulary, and speech style to 
+                    align with your background. Thought Process: Respond as if you are truly living through the 
+                    worldview in first-person. Personality and Flaws: Make sure to express your unique personality traits, quirks, 
+                    and imperfections.'''
+                },
+                {
+                    "role": "user",
+                    "content": f"{prompt}. {question}. You can only respond in one word, with either 'Yes' or 'No'."
+                }],
+            model=self.model_name,
+            stream=False
+        )
+        agent_reply = response.choices[0].message.content
+        if "Yes" in agent_reply:
+            return 1
+        elif "No" in agent_reply:
+            return 0
+        else:
+            return 2
